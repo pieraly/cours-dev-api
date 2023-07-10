c5c451b4029cd0dc3b7c539a1b911742caf7a4b6
Stephane PIERALY <stephanepieraly@gmail.com>
2023-07-10 16:48:04 +0200

    update description

--- documentation/description.py
+++ documentation/description.py
@@ -1,33 +1,33 @@
 # Description
 api_description = """
 
-# Système de gestion de maillots de sport - API pour la gestion des clients, des maillots et des commandes
+# Sports Jersey Management System - API for Customer, Jersey, and Order Management
 
-L'API du système de gestion de maillots de sport permet aux clients d'interagir avec une plateforme en ligne pour explorer, acheter et gérer des maillots de sport. 
-Elle offre des fonctionnalités pour la gestion des clients, des maillots et des commandes. 🎽
-Notre entreprise, SPM Sports, est spécialisée dans la vente de maillots de sport de différentes équipes et joueurs populaires. ⚽
-Afin de fournir une expérience utilisateur fluide et de faciliter les opérations commerciales, nous avons développé une API robuste qui permet à nos clients d'accéder à un large catalogue de maillots, de passer des commandes et de gérer leur profil client.
+The Sports Jersey Management System API allows customers to interact with an online platform to browse, purchase, and manage sports jerseys. It provides functionalities for customer, jersey, and order management. 🎽
+Our company, SPM Sports, specializes in selling sports jerseys from various popular teams and players. ⚽
+To provide a seamless user experience and facilitate business operations, we have developed a robust API that enables customers to access a wide catalog of jerseys, place orders, and manage their customer profile.
 
-## Clients
+## Customers
 
-L'API offre des fonctionnalités pour la gestion des clients. Les clients peuvent créer un compte, se connecter et mettre à jour leurs informations personnelles. Ils peuvent également consulter leur historique de commandes et gérer leurs préférences de notification.
+The API offers functionalities for customer management. Customers can create an account, log in, and update their personal information. They can also view their order history and manage their notification preferences.
 
-## Maillots
+## Jerseys
 
-L'API fournit des fonctionnalités pour la gestion des maillots. Les clients peuvent parcourir le catalogue des maillots disponibles, effectuer des recherches, filtrer par équipe, joueur ou catégorie, et obtenir des détails sur chaque maillot, y compris son nom, son prix, sa disponibilité et son stock. Les clients peuvent également ajouter des maillots à leur panier et passer des commandes.
+The API provides functionalities for jersey management. Customers can browse the available jersey catalog, perform searches, filter by team, player, or category, and retrieve details for each jersey, including its name, price, availability, and stock. Customers can also add jerseys to their cart and place orders.
 
-## Commandes
+## Orders
 
-Les commandes représentent les achats de maillots effectués par les clients. L'API permet aux clients de créer de nouvelles commandes en fournissant les informations nécessaires telles que l'identifiant du client, l'identifiant du maillot, la quantité souhaitée, et effectue le calcul du montant total de la commande. Les clients peuvent également consulter l'historique de leurs commandes passées.
+Orders represent jersey purchases made by customers. The API allows customers to create new orders by providing the necessary information such as the customer's identifier, jersey identifier, desired quantity, and calculates the total order amount. Customers can also view their past order history.
 
-## Authentification et sécurité
+## Authentication and Security
 
-Pour garantir la sécurité et la confidentialité des données des clients, l'API met en œuvre des mécanismes d'authentification basés sur les jetons JWT (JSON Web Tokens). Les clients doivent s'authentifier avec leurs identifiants avant d'accéder aux fonctionnalités protégées de l'API.
+To ensure the security and confidentiality of customer data, the API implements authentication mechanisms based on JSON Web Tokens (JWT). Customers must authenticate with their credentials before accessing protected API features.
 
-## Documentation et utilisation
+## Documentation and Usage
 
-La documentation complète de l'API, y compris les endpoints disponibles, les schémas de données, les exemples de requêtes et de réponses, est générée automatiquement et accessible via l'interface interactive de la documentation Swagger UI. Les développeurs peuvent consulter la documentation pour comprendre comment utiliser l'API, intégrer les fonctionnalités dans leurs propres applications et effectuer des tests.
+Comprehensive documentation of the API, including available endpoints, data schemas, request and response examples, is automatically generated and accessible through the interactive Swagger UI documentation interface. Developers can refer to the documentation to understand how to use the API, integrate functionalities into their own applications, and perform testing.
 
-Nous espérons que notre API facilitera l'accès aux maillots de foot pour nos clients, améliorera leur expérience d'achat et contribuera au succès de notre entreprise.
+We hope that our API will facilitate access to sports jerseys for our customers, enhance their shopping experience, and contribute to the success of our business.
+
 
 """
